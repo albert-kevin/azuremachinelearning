dc450c9e6ae64ee9e178a03125909724bd7f4a63
Kevin ALBERT <beire_@hotmail.com>
2020-03-22 10:39:19 +0100

    add comments and more explanations

--- code/databricks/mount.py
+++ code/databricks/mount.py
@@ -1,5 +1,15 @@
 # Databricks notebook source
+# MAGIC %md
+# MAGIC Check the current mount points already active
+
+# COMMAND ----------
+
 # MAGIC %fs mounts
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC Example parameters, to show how it looks
 
 # COMMAND ----------
 
@@ -7,9 +17,14 @@ adlsGen2AccountName = "datalake21032020"
 adlsGen2Key = "Ck/4hMq3Zrzq5toZ96zE6cDncjbw2VdkR9ny1xXA3GLBwQXIv7V1ycSc/KpqyNRcoPWKtzKljjpcZVqjWOu+3Q=="
 tenantId = "73b49191-8db3-45ab-87b3-b8f956ac123b"
 clientId = "38c02221-4a41-4ec8-b8da-a81f16c38e82"
-clientKey = "l]ABG6@Z/9r/hX7EK0zavK5Nx[MA-J1V"
+clientKey = "l]ABG6@Z/9r/chRfgoSdklmDR[MA-J1V"
 fileSystemName = "datalake"
 mount_dir = "/mnt/adlsGen2"
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC Mount your ADLS Gen2 account with OAuth to DBFS, using the `configs`  
 
 # COMMAND ----------
 
@@ -28,7 +43,30 @@ dbutils.fs.mount(
 
 # COMMAND ----------
 
+# MAGIC %md
+# MAGIC ADLS Gen2 has hierarchical namespace enabled to organize objects/files into a hierarchy of directories for efficient data access  
+# MAGIC You cannot use the `wasb` or `wasbs` scheme to access the `blob.core.windows.net` endpoint  
+# MAGIC It now will become a `abfs` or `abfss` mount point   
+# MAGIC You must initialize a file system once and this code is included in `Cluster Config`  
+# MAGIC   
+# MAGIC `spark.serializer org.apache.spark.serializer.KryoSerializer`  
+# MAGIC `spark.hadoop.fs.azure.createRemoteFileSystemDuringInitialization true`  
+# MAGIC `spark.hadoop.fs.azure.account.key.datalake21032020.dfs.core.windows.net Ck/4hMq3Zrzq5toZ96zE6cDncjbw2VdkR9ny1xXA3GLBwQXIv7V1ycSc/KpqyNRcoPWKtzKljjpcZVqjWOu+3Q==`  
+# MAGIC `spark.databricks.delta.preview.enabled true`
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC verify new mount
+
+# COMMAND ----------
+
 # MAGIC %fs mounts
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC remove mount (if needed)
 
 # COMMAND ----------
 
